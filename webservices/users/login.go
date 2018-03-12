85f7ca179a113640f12ed31602723597b1bef7e5
Vilius <vilius@rinkodara.lt>
2018-03-12 20:34:22 +0200

    Login service update

--- webservices/users/login.go
+++ webservices/users/login.go
@@ -4,47 +4,36 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"os"
 )
 
 type Payload struct {
-	Response map[string]string
+	ErrorMessage string
 	ReturnCode int
 }
 
-func Login(w http.ResponseWriter, r *http.Request)(bool){
-
-	return true
-}
-
 func main() {
-	http.HandleFunc("/login", ServRest)
+	http.HandleFunc("/", Login)
 	http.ListenAndServe("localhost:1338", nil)
-	fmt.Scanln()
-	os.Exit(0)
 }
 
-func ServRest(w http.ResponseWriter, r *http.Request) {
+func Login(w http.ResponseWriter, r *http.Request) {
 
 	r.ParseForm()                     // Parses the request body
+
 	username := r.Form.Get("login") // x will be "" if parameter is not set
 	password := r.Form.Get("password") // x will be "" if parameter is not set
-	fmt.Println(username)
-	fmt.Println(password)
 
-	response, err := getJsonResponse()
-	if err != nil {
-		panic(err)
+
+
+	ErrorMsg := "Prisijungta Sėkmingai"
+	ErrorCode := 0
+
+	if(username != "vilius" || password != "slaptazodis") {
+		ErrorMsg := "Blogi prisijungimo duomenys"
+		ErrorCode := 1
 	}
 
-	fmt.Fprintf(w, string(response))
-}
+	p := Payload{ErrorMsg, ErrorCode}
 
-func getJsonResponse()([]byte, error){
-	data := make(map[string]string)
-	data["username"] = "threatx"
-
-	p := Payload{data, 0}
-
-	return json.MarshalIndent(p, "", "  ")
+	fmt.Fprintf(w, string(json.MarshalIndent(p, "", "  ")))
 }
