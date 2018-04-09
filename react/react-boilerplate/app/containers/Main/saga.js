a0df44831f47c8cba2c0788e8ab08146cbd4a01e
Vilius <vilius@rinkodara.lt>
2018-04-09 22:19:24 +0300

    main saga

--- react/react-boilerplate/app/containers/Main/saga.js
+++ react/react-boilerplate/app/containers/Main/saga.js
@@ -5,44 +5,23 @@
 import { call, put, select, takeLatest } from 'redux-saga/effects';
 
 import request from 'utils/request';
-import {makeSelectPassword, makeSelectUsername} from 'containers/LogInPage/selectors';
-import {LOGIN} from "./constants";
-import {loginSuccess, requestError} from "./actions";
 import {SESSIONID} from "../App/constants";
 
-/**
- * Login request handler
- */
-export function* loginRequest() {
-
-  // Select username and password from store
-  const username = yield select(makeSelectUsername());
-  const password = yield select(makeSelectPassword());
-  const requestURL = `http://localhost:1338`;
-
-  try {
-    // Call our request helper (see 'utils/request')
-    const response = yield call(request, requestURL, {username: username, password: password});
-    yield put(loginSuccess(response));
-  } catch (err) {
-    yield put(requestError(err));
-  }
-}
 
 /**
  * Root saga manages watcher lifecycle
  */
 export default function* checkLoginState() {
-
+  console.log('asd');
     const sessionID = sessionStorage.getItem(SESSIONID);
 
       const requestURL = `http://localhost:1337`;
       const requestData = {
-        session {
+        session: {
           id :sessionID
         }
       };
-      
+
         try {
           // Call our request helper (see 'utils/request')
           const response = yield call(request, requestURL, requestData);
